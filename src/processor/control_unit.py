fd442d7b12fbc004f988e84f2b55396b426f27d1
n-bolanos <nianbolanos@gmail.com>
2026-03-05 22:40:35 -0500

    creacion de jmp condicionales

--- src/processor/control_unit.py
+++ src/processor/control_unit.py
@@ -227,7 +227,7 @@ class ControlUnit(MicroinstructionMixin):
             initial_pos = int.from_bytes(self._dp, byteorder='little', signed=False) * 4
             final_pos = initial_pos + self._mode_length[mode]
             cod_i = self._to_binary(self._ir, 64, False)[initial_pos: final_pos]
-            if mode == "r":
+            if mode == "r" or mode == "n":
                 ops[i] = self._registers[int(cod_i, 2)]
 
             elif mode == "i":
@@ -235,10 +235,7 @@ class ControlUnit(MicroinstructionMixin):
             
             elif mode == "m":
                 ops[i] = bytearray(int(cod_i, 2).to_bytes(8, byteorder='little', signed=True)) # ¿Por qué no int?
-            
-            elif mode == "n": # ¿Por qué no int?^^
-                self._mar[:] = self._registers[int(cod_i, 2)]
-                ops[i] = self._mdr[:]
+    
 
             acc = self._registers[15]
             self.add_ra(self._dp, bytearray((self._mode_length[mode]//4).to_bytes(8, byteorder='little', signed=True)))
